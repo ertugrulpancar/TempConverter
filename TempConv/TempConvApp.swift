f6f41fb6a53c48ecacf828c33c0a33a1c495e838
Ertuğrul Pancar <21220030023@mersin.edu.tr>
2024-12-02 15:31:16 +0300

    Initial Commit

---
+++ TempConv/TempConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvApp.swift
+//  TempConv
+//
+//  Created by Ertuğrul Pancar on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TempConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
